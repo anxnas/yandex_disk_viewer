336c9c8c1c520ba4f034ec4fdfad964d01cdb66f
anxnas <106204348+anxnas@users.noreply.github.com>
2024-08-29 00:46:30 +0300

    Add form for public key input and update index view

--- undisk/views.py
+++ undisk/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from .forms import PublicLinkForm
 
 class IndexView(View):
     def get(self, request):
-        return render(request, 'undisk/index.html')
+        form = PublicLinkForm()
+        return render(request, 'undisk/index.html', {'form': form})
+
+    def post(self, request):
+        form = PublicLinkForm(request.POST)
+        if form.is_valid():
+            public_key = form.cleaned_data['public_key']
+            return redirect('index')  # Перенаправление на ту же страницу с параметром public_key
+        return render(request, 'undisk/index.html', {'form': form})
